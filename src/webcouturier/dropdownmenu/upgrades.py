38856c4f8be03573cfe5e9911d9d5e62e82280f9
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-01-23 21:33:13 +0100

    provide migration to fix profile diff: order skin layers in sunburst

--- src/webcouturier/dropdownmenu/upgrades.py
+++ src/webcouturier/dropdownmenu/upgrades.py
@@ -1,7 +1,21 @@
+from Products.CMFCore.utils import getToolByName
 
 def upgrade_to_1000(context):
     pass
 
 def upgrade_1000_to_1010(context):
-    pass
-    #TODO: upgrade skin directory
+    """If dropdownmenu_sunburst is after dropdownmenu in sunburst skin, 
+    reorder them"""
+    skin = getToolByName(context, 'portal_skins')
+    layers = skin.getSkinPath('Sunburst Theme').split(',')
+    dds = layers.index('dropdownmenu_sunburst')
+    dd  = layers.index('dropdownmenu')
+    if dds>dd:
+        #switch them
+        layers[dd] ='dropdownmenu_sunburst'
+        layers[dds]='dropdownmenu'
+        path = ','.join(layers)
+        skin.testSkinPath(path)
+        sels = skin._getSelections()
+        sels['Sunburst Theme'] = path
+
