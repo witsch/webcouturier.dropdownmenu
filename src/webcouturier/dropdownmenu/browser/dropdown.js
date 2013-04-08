12bcc89a742be40fb625f706c5fdf68acf53773f
Ross Patterson <me@rpatterson.net>
2013-04-08 16:57:45 -0700

    Restore mouseover and clickable folders for bootstrap

--- src/webcouturier/dropdownmenu/browser/dropdown.js
+++ src/webcouturier/dropdownmenu/browser/dropdown.js
@@ -6,3 +6,11 @@ jQuery(function ($) {
         e.preventDefault();
     });
 });
+
+jQuery('ul.nav li.dropdown').hover(function() {
+    jQuery(this).closest('.dropdown-menu').stop(true, true).show();
+    jQuery(this).addClass('open');
+}, function() {
+    jQuery(this).closest('.dropdown-menu').stop(true, true).hide();
+    jQuery(this).removeClass('open');
+});
