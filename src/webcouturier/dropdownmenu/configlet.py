c2214db68733cb5283cc54329168706dc4fa97b8
Pawel Lewicki <lewicki@provider.pl>
2015-10-17 11:03:14 +0200

    Configlet definition ported to RegistryEditForm

--- src/webcouturier/dropdownmenu/configlet.py
+++ src/webcouturier/dropdownmenu/configlet.py
@@ -1,40 +1,22 @@
 # -*- coding: utf-8 -*-
-from Products.CMFCore.interfaces import IPropertiesTool
-from Products.CMFDefault.formlib.schema import ProxyFieldProperty
-from Products.CMFDefault.formlib.schema import SchemaAdapterBase
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-from plone.app.controlpanel.form import ControlPanelForm
-from zope.component import adapts, getUtility
-from zope.formlib.form import FormFields
-from zope.interface import implements
+from Products.CMFPlone import PloneMessageFactory as _
 from webcouturier.dropdownmenu.browser.interfaces import IDropdownConfiguration
-from webcouturier.dropdownmenu import msg_fact as _
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
 
 
-class DropdownControlPanelAdapter(SchemaAdapterBase):
-    adapts(IPloneSiteRoot)
-    implements(IDropdownConfiguration)
+class DropdownControlPanelForm(RegistryEditForm):
 
-    def __init__(self, context):
-        super(DropdownControlPanelAdapter, self).__init__(context)
-        self.context = getUtility(IPropertiesTool).dropdown_properties
+    id = "DropdownControlPanel"
+    schema = IDropdownConfiguration
+    schema_prefix = "addons"
 
-    dropdown_depth = ProxyFieldProperty(
-        IDropdownConfiguration['dropdown_depth'])
-    enable_caching = ProxyFieldProperty(
-        IDropdownConfiguration['enable_caching'])
-    enable_parent_clickable = ProxyFieldProperty(
-        IDropdownConfiguration['enable_parent_clickable'])
-    enable_desc = ProxyFieldProperty(
-        IDropdownConfiguration['enable_desc'])
-    enable_thumbs = ProxyFieldProperty(
-        IDropdownConfiguration['enable_thumbs'])
-
-
-class DropdownControlPanel(ControlPanelForm):
-    form_fields = FormFields(IDropdownConfiguration)
-
-    label = _(u"A dropdown menu configuration.")
+    label = _(u"A dropdown menu configuration.", default=u"A dropdown menu configuration.")
     description = _(
-        u'Settings to configure dropdown menus for global navigation.')
-    form_name = _(u'Dropdown menu settings')
+        u"Settings to configure dropdown menus for global navigation.",
+        default=u"Settings to configure dropdown menus for global navigation."
+    )
+
+
+class DropdownControlPanel(ControlPanelFormWrapper):
+    form = DropdownControlPanelForm
